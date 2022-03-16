c3fc3cc36166b54ab0497b0f1cb7e1efda24d9b1
danibrilyan <dani@brilyan.com>
2022-03-16 17:58:33 +0700

    update web

---
+++ components/firebase.js
@@ -0,0 +1,17 @@
+<script>
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-analytics.js";
+  const firebaseConfig = {
+    apiKey: "AIzaSyCdpBIdigGq1ew-zcfxdepxpbZNP3MOzC4",
+    authDomain: "webportal-b0d16.firebaseapp.com",
+    projectId: "webportal-b0d16",
+    storageBucket: "webportal-b0d16.appspot.com",
+    messagingSenderId: "984212010418",
+    appId: "1:984212010418:web:76a5021e0e3c029647e0ab",
+    measurementId: "G-7QLBQL6EJ7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
